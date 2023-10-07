837cf9b3ef45aad6bb4909eb667cb12d1b5b5118
NghiaOtaku <otaku.nghia11227@gmail.com>
2023-10-08 01:44:35 +0700

    add some features

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+let bodyParser = require("body-parser");
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+mongoose.connect(process.env["MONGO_URI"], {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
+console.log("DB connected!!!");
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
+const UserSchema = new mongoose.Schema({
+  username: String,
+});
 
+let User = mongoose.model("User", UserSchema);
+
+app.post("/api/users", async (req, res) => {
+  let addUser = new User({ username: req.body.username });
+  addUser.save();
+  res.json(addUser);
+});
+
+app.get("/api/users", async (req, res) => {
+  let allUsers = await User.find({});
+  res.json(allUsers);
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
